2595e018a0e1991e01d5999997b31146104db50a
Rishabh <chaudharyrishabh029@gmail.com>
2024-12-17 09:00:58 +0530

    added new home image

--- components/swiper/SwiperOne.tsx
+++ components/swiper/SwiperOne.tsx
@@ -29,10 +29,10 @@ export default function SwiperOne() {
                 className="mySwiper"
             >
                 <SwiperSlide><article className="relative isolate flex flex-col justify-end overflow-hidden rounded-3xl px-8 pt-44 max-w-sm mx-auto">
-                    <Image src="https://i.imgur.com/t0WW0h2.png" width={600} height={600} alt="Rishabh Portfolio Home" className="absolute inset-0 h-full w-full object-cover" />
-                    <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                    <h3 className="z-10 mt-3 text-lg font-bold text-white">Rishabh Chaudhary</h3>
-                    <div className="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Software Developer</div>
+                    <Image src="/rishabh-home-img.png" width={700} height={700} alt="Rishabh Portfolio Home" className="absolute inset-0 h-full w-full object-cover" />
+                    <div className="absolute inset-0 bg-gradient-to-t from-blue-300/60 via-white/10"></div>
+                    <h3 className="z-10 mt-3 text-lg font-bold bg-blue-300/10 w-3/4 backdrop-blur-2xl rounded-2xl pl-2 text-gray-700">Rishabh Chaudhary</h3>
+                    <div className="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-500">Software Developer</div>
                 </article></SwiperSlide>
                 <SwiperSlide>Slide 2</SwiperSlide>
                 <SwiperSlide>Slide 3</SwiperSlide>
