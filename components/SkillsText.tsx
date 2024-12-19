5c43e69e9286b248374fb8a305e2ac0e6638a9ef
Rishabh <chaudharyrishabh029@gmail.com>
2024-12-19 09:29:32 +0530

    minor changes

--- components/SkillsText.tsx
+++ components/SkillsText.tsx
@@ -6,19 +6,25 @@ const SkillsText = () => {
     return (
         <>
             <div className="mx-auto w-80 grid grid-cols-2">
-                <div className="grid grid-cols-4 w-36 mt-5 border gap-2 text-4xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-8 animate-border-gradient">
-                    <div className="icon-glow" title="Next.js"><FaDocker /></div>
-                    <div className="icon-glow" title="MySQL"><SiMysql /></div>
+                <div className="w-36 mt-5 border gap-2 text-4xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-4 animate-border-gradient">
+                    <div className="text-base font-bold" title="Next.js">DSA</div>{/* make a vertical slider here with all the skills */}
+                    <div className="font-bold" title="MySQL"><SiMysql /></div>
                 </div>
-                <div className="grid grid-cols-4 w-40 mt-5 border gap-4 text-3xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-8 animate-border-gradient">
-                    <div className="icon-glow" title="Next.js"><FaDocker /></div>
-                    <div className="icon-glow" title="MySQL"><SiMysql /></div>
+                <div className="w-40 mt-5 border gap-4 text-3xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-4 animate-border-gradient">
+                    <div className="text-lg font-bold text-center" title="MySQL">Full-Stack</div>
+                    <div className="text-lg font-bold text-center" title="MySQL">Devops</div>
+                    <div className="text-lg font-bold text-center" title="Next.js">Cloud</div>
                 </div>
             </div>
             <div className="mx-auto w-80 grid grid-cols-2">
-                <div className="grid grid-cols-4 w-80 mt-5 border gap-4 text-3xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-8 animate-border-gradient">
-                    <div className="icon-glow" title="Next.js"><FaDocker /></div>
-                    <div className="icon-glow" title="MySQL"><SiMysql /></div>
+                <div className="grid grid-cols-5 w-80 mt-4 border gap-5 text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-4 animate-border-gradient">
+                    <div className="font-sm font-bold" title="Next.js">SEO</div>
+                    <div className="font-sm font-bold" title="Next.js">ReactJs</div>
+                    <div className="font-sm font-bold" title="Next.js">NextJs</div>
+                    <div className="font-sm font-bold" title="Next.js">Jenkins</div>
+                    <div className="font-sm font-bold" title="Next.js">AWS</div> {/* make a horizontal slider here with all the skills */}
+                    <div className="font-sm font-bold" title="Next.js">NextJs</div>
+                    <div className="font-sm font-bold" title="Next.js">Tailwind</div>
                 </div>
             </div>
 
