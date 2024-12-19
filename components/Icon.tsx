9c0871c865a83f388f01700838633189b1d95b22
Rishabh <chaudharyrishabh029@gmail.com>
2024-12-19 09:03:09 +0530

    made skills colourfull

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { FaReact, FaNodeJs, FaAws, FaDocker } from "react-icons/fa"; // Font Awesome
-import { SiNextdotjs, SiTypescript, SiJavascript, SiExpress, SiMongodb, SiRedux, SiJenkins, SiKubernetes, SiTailwindcss, SiBootstrap, SiPostgresql, SiMysql } from "react-icons/si"; // Simple Icons
+import { FaReact, FaNodeJs, FaAws, FaDocker } from "react-icons/fa";
+import { SiNextdotjs, SiTypescript, SiJavascript, SiExpress, SiMongodb, SiRedux, SiJenkins, SiKubernetes, SiTailwindcss, SiBootstrap, SiPostgresql, SiMysql } from "react-icons/si";
 
 const TechStackIcons = () => {
     return (
-        <div className="grid grid-cols-4 w-80 mx-auto mt-24 border gap-4 text-4xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-8 animate-border-gradient">
-            <div className="icon-glow" title="ReactJS"><FaReact /></div>
-            <div className="icon-glow" title="Next.js"><SiNextdotjs /></div>
-            <div className="icon-glow" title="TypeScript"><SiTypescript /></div>
-            <div className="icon-glow" title="JavaScript"><SiJavascript /></div>
-            <div className="icon-glow" title="Express.js"><SiExpress /></div>
-            <div className="icon-glow" title="Node.js"><FaNodeJs /></div>
-            <div className="icon-glow" title="MySQL"><SiMysql /></div>
-            <div className="icon-glow" title="MongoDB"><SiMongodb /></div>
-            <div className="icon-glow" title="Redux"><SiRedux /></div>
-            <div className="icon-glow" title="Jenkins"><SiJenkins /></div>
-            <div className="icon-glow" title="AWS"><FaAws /></div>
-            <div className="icon-glow" title="Kubernetes"><SiKubernetes /></div>
-            <div className="icon-glow" title="Docker"><FaDocker /></div>
-            <div className="icon-glow" title="TailwindCSS"><SiTailwindcss /></div>
-            <div className="icon-glow" title="Bootstrap"><SiBootstrap /></div>
-            <div className="icon-glow" title="PostgreSQL"><SiPostgresql /></div>
+        <div className="grid grid-cols-4 w-80 mx-auto mt-24 border gap-4 text-4xl border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-6">
+            <div className="icon-glow react-icon" title="ReactJS"><FaReact /></div>
+            <div className="icon-glow nextjs-icon" title="Next.js"><SiNextdotjs /></div>
+            <div className="icon-glow ts-icon" title="TypeScript"><SiTypescript /></div>
+            <div className="icon-glow js-icon" title="JavaScript"><SiJavascript /></div>
+            <div className="icon-glow express-icon" title="Express.js"><SiExpress /></div>
+            <div className="icon-glow node-icon" title="Node.js"><FaNodeJs /></div>
+            <div className="icon-glow mysql-icon" title="MySQL"><SiMysql /></div>
+            <div className="icon-glow mongodb-icon" title="MongoDB"><SiMongodb /></div>
+            <div className="icon-glow redux-icon" title="Redux"><SiRedux /></div>
+            <div className="icon-glow jenkins-icon" title="Jenkins"><SiJenkins /></div>
+            <div className="icon-glow aws-icon" title="AWS"><FaAws /></div>
+            <div className="icon-glow kubernetes-icon" title="Kubernetes"><SiKubernetes /></div>
+            <div className="icon-glow docker-icon" title="Docker"><FaDocker /></div>
+            <div className="icon-glow tailwind-icon" title="TailwindCSS"><SiTailwindcss /></div>
+            <div className="icon-glow bootstrap-icon" title="Bootstrap"><SiBootstrap /></div>
+            <div className="icon-glow postgresql-icon" title="PostgreSQL"><SiPostgresql /></div>
         </div>
     );
 };
