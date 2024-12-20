7214d4275a1902c761c4e1860fa2dd8313880a24
Rishabh <chaudharyrishabh029@gmail.com>
2024-12-20 05:58:13 +0530

    minor change skills

--- components/SkillsText.tsx
+++ components/SkillsText.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FaReact, FaNodeJs, FaAws, FaDocker } from "react-icons/fa"; // Font Awesome
-import { SiNextdotjs, SiTypescript, SiJavascript, SiExpress, SiMongodb, SiRedux, SiJenkins, SiKubernetes, SiTailwindcss, SiBootstrap, SiPostgresql, SiMysql } from "react-icons/si"; // Simple Icons
 
 const SkillsText = () => {
     return (
@@ -8,12 +6,14 @@ const SkillsText = () => {
             <div className="mx-auto w-80 grid grid-cols-2">
                 <div className="w-36 mt-5 border gap-2 text-4xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-4 animate-border-gradient">
                     <div className="text-base font-bold" title="Next.js">DSA</div>{/* make a vertical slider here with all the skills */}
-                    <div className="font-bold" title="MySQL"><SiMysql /></div>
                 </div>
-                <div className="w-40 mt-5 border gap-4 text-3xl text-blue-500 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-4 animate-border-gradient">
-                    <div className="text-lg font-bold text-center" title="MySQL">Full-Stack</div>
-                    <div className="text-lg font-bold text-center" title="MySQL">Devops</div>
-                    <div className="text-lg font-bold text-center" title="Next.js">Cloud</div>
+                <div className="w-40 mt-5 border gap-4 text-3xl text-blue-500/80 border-blue-300 rounded-3xl shadow-2xl shadow-blue-300 bg-white p-4 animate-border-gradient">
+                    <ol>
+                        <li className="text-base font-bold text-shadow">Full-Stack</li>
+                        <li className="text-base font-bold text-shadow">DevOps</li>
+                        <li className="text-base font-bold text-shadow">System Design</li>
+                        <li className="text-base font-bold text-shadow">Cloud</li>
+                    </ol>
                 </div>
             </div>
             <div className="mx-auto w-80 grid grid-cols-2">
