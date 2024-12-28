b8ecb2d77f682f3327b3bf9c32b43a84ac59d38b
Rishabh <chaudharyrishabh029@gmail.com>
2024-12-28 09:14:23 +0530

    added slider on homepage

--- components/HomeAbout.tsx
+++ components/HomeAbout.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import HorizontalSlider from './autoslider/HorizontalSlider';
+import { FaComments, FaUsers, FaTools, FaLaptopCode, FaClock, FaClipboardList, FaShieldAlt, FaBrain, FaHandsHelping } from 'react-icons/fa';
+import HomeHorizontalSlider from '@/modules/HomeModule/HomeHorizontalSlider';
 
 const HomeAbout = () => {
+    const Skills = [
+        { name: "Communication", icon: <FaComments /> },
+        { name: "Team Collaboration", icon: <FaUsers /> },
+        { name: "Adaptability", icon: <FaTools /> },
+        { name: "Problem-Solving", icon: <FaLaptopCode /> },
+        { name: "Time Management", icon: <FaClock /> },
+        { name: "Work Ethic", icon: <FaClipboardList /> },
+        { name: "Resilience", icon: <FaShieldAlt /> },
+        { name: "Critical Thinking", icon: <FaBrain /> },
+        { name: "Empathy", icon: <FaHandsHelping /> },
+    ];
+
     return (
-        <div className='w-80 mx-auto max-w-sm '>
-            <p className='text-black font-bold text-base'>Hii, I am Rishabh, Software Developer Expert in Devops and Cloud</p>
-            <p></p>
+        <div className='w-80 mx-auto max-w-sm'>
+            <HomeHorizontalSlider items={Skills.map(skill => skill.icon)} />
+            <p className='text-black font-bold text-base'>
+                Hii, I am Rishabh, Software Developer Expert in DevOps and Cloud
+            </p>
         </div>
     )
 }
 
-export default HomeAbout
+export default HomeAbout;
---
+++ modules/HomeModule/HomeHorizontalSlider.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+type HomeHorizontalSliderProps = {
+    items: any;
+};
+
+const HomeHorizontalSlider: React.FC<HomeHorizontalSliderProps> = ({ items }) => {
+    const horizontalSettings = {
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: "linear",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    };
+
+    return (
+        <>
+            <Slider {...horizontalSettings}>
+                {items.map((item: any, index: any) => (
+                    <div key={index} className="text-center text-xl font-semibold text-black text-shadow1">
+                        {item}
+                    </div>
+                ))}
+            </Slider>
+        </>
+    );
+};
+
+export default HomeHorizontalSlider;
